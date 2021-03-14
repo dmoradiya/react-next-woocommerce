e4845e6af870602bd6071beb408397e1a6eefdbd
dhmec <dharmoradiya@gmail.com>
2021-03-14 17:50:01 -0600

    Add server.js;wooConfig.js

---
+++ server.js
@@ -0,0 +1,48 @@
+// create custom server
+
+const next = require( 'next' );
+const express = require( 'express' );
+const wooConfig = require('./wooConfig');
+
+// Woocommerce Api keys
+var WooCommerceAPI = require('woocommerce-api');
+ 
+var WooCommerce = new WooCommerceAPI({
+  url: wooConfig.siteUrl,
+  consumerKey: wooConfig.consumerKey,
+  consumerSecret: wooConfig.consumerSecret,
+  wpAPI: true,
+  version: 'wc/v1'
+});
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+
+// Route not defined by express should be handled by next js
+const handle = app.getRequestHandler()
+
+//Prepare our App
+app.prepare().then(() => {
+    const server = express();
+    // Create Route to display all products
+    server.get('/products', (req, response) => {
+        WooCommerce.get("products", function(err, data, res ) {
+            response.json( JSON.parse(res) );
+        });
+      
+    });
+
+
+    server.get( '*', (req, res) => {
+        return  handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`Ready on http://localhost:${port}`);
+    })   
+}).catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+});
---
+++ wooConfig.js
@@ -0,0 +1,6 @@
+const wooConfig = {
+    siteUrl:'http://woo.unaux.com/',
+    consumerKey: 'ck_2f3105f064e9a4e1655313dfbc9d3857d68f1850',
+    consumerSecret: 'cs_90c226439451e3639f68e64c392e190d8ca72320'
+};
+module.exports = wooConfig;
