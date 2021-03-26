3701444c886115acfff7bd8827b418b59ece4367
dhmec <dharmoradiya@gmail.com>
2021-03-25 23:26:04 -0600

    Add REST API data on product page

--- components/Products.js
+++ components/Products.js
@@ -1,6 +1,18 @@
+
 const Product = ( props ) => {
+
+    const { product } = props;
     return (
-        <div>Product</div>
+        <div className="card mb-3" >
+            <h3 className="card-header">{ product.name }</h3>
+            <img width="400" height="400"
+                src={ product.images[0].src }
+                alt="Product image"/>
+            <div className="card-body">
+                <h5 className="card-subtitle text-muted">{ product.price }</h5>
+            </div> 
+
+        </div>
     );
 }
 export default Product;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Layout from "../components/Layout";
 import clientConfig from '../client-config';
 import fetch from 'isomorphic-unfetch';
+import Product from "../components/Products";
 const Index = ( props) => {
+    // check object in console
     console.warn(props);
+
+    const { products } = props;
+
     return (
         <Layout>
-            <div>Hi dharmesh</div>
+            { products.length ? (
+                products.map( product => <Product  product={ product } key={ product.id } /> )
+            ) : '' }
         </Layout>
 
     );
 }
+// getIntialProps runs server side and client side
 Index.getInitialProps = async () => {
 const res = await fetch(`${clientConfig.siteUrl}/products`);
 const productData = await res.json();
