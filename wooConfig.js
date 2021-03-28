751de75e6891b702d0ad8db1ac3d092bbb7b9838
dhmec <dharmoradiya@gmail.com>
2021-03-28 14:50:22 -0600

    Add graphql url in wooConfig.js

--- wooConfig.js
+++ wooConfig.js
@@ -1,6 +1,7 @@
 const wooConfig = {
     siteUrl:'http://localhost/woo-next/',
     consumerKey: 'ck_243fd4d4068cbfb572eaf0fcd5248d7d754f5213',
-    consumerSecret: 'cs_ed6b048934322f82e6fcc94a1bfb6b1f81005ddd'
+    consumerSecret: 'cs_ed6b048934322f82e6fcc94a1bfb6b1f81005ddd',
+    graphqlUrl: 'http://localhost/woo-next/graphql'
 };
 module.exports = wooConfig;
