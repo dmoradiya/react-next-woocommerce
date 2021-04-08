821bd2f56f498b777ddb4b8b4ad70736e45ad3c6
dhmec <dharmoradiya@gmail.com>
2021-04-07 22:08:46 -0600

    Layout component wraped with AppContext component

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,21 @@
 import Head from 'next/head';
+import { AppContext } from './context/AppContext';
 import Header from './Header';
 
 
 const Layout = ( props ) => {
     return (
-        <div>
-            <Head>
-                <title>React-Next-Woocommerce</title>
-                <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css"/>                                  
-            </Head>
-           <Header/>                
+        <AppContext>
+            <div>
+                <Head>
+                    <title>React-Next-Woocommerce</title>
+                    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css"/>                                  
+                </Head>
+            <Header/>                
             { props.children }
-        </div>
+            </div>
+        </AppContext>
+        
     );
 }
 export default Layout;
